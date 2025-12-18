283bacba921050087252f69c7f0f100944644b44
Max Heyer <mail@max.gp>
2025-12-18 13:37:25 +0100

    Set vivaldi as default webbrowser

--- home.nix
+++ home.nix
@@ -41,6 +41,17 @@
       "waybar".source = ./configs/waybar;
     };
 
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "vivaldi.desktop";
+        "x-scheme-handler/http" = "vivaldi.desktop";
+        "x-scheme-handler/https" = "vivaldi.desktop";
+        "x-scheme-handler/about" = "vivaldi.desktop";
+        "x-scheme-handler/unknown" = "vivaldi.desktop";
+      };
+    };
+
     gtk = {
       enable = true;
       iconTheme = {
