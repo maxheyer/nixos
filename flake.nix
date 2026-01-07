ac416486a8c00c317109fae6c1c2800a24c86df3
Max Heyer <mail@max.gp>
2026-01-07 12:40:49 +0100

    Add backup file extension

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
           {
              home-manager.useGlobalPkgs = true;
              home-manager.useUserPackages = true;
+             home-manager.backupFileExtension = "backup";
              home-manager.users.max = import ./home.nix;
            }
            chaotic.nixosModules.default
