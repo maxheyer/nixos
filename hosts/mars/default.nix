62b8d21119642e49271d60afa47fe4b6cd66c5b4
Max Heyer <mail@max.gp>
2026-02-01 03:29:32 +0100

    Update

--- hosts/mars/default.nix
+++ hosts/mars/default.nix
@@ -64,14 +64,86 @@ hardware.graphics = {
     package = pkgs.ollama-cuda;
   };
 
-  services.open-webui = {
-    enable = true;
-    environment = {
-      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-    };
-  };
-
   environment.systemPackages = with pkgs; [
     discord-ptb
+    (pkgs.ollama.override { 
+      acceleration = "cuda";
+    })
   ];
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
+
+  services.snapper.configs.home = {
+    SUBVOLUME = "/home";
+
+    TIMELINE_CREATE = true;
+    TIMELINE_CLEANUP = true;
+
+    TIMELINE_MIN_AGE = "1800";
+    TIMELINE_LIMIT_HOURLY = "24";
+    TIMELINE_LIMIT_DAILY = "7";
+    TIMELINE_LIMIT_WEEKLY = "4";
+    TIMELINE_LIMIT_MONTHLY = "0";
+
+    IGNORE_PATTERNS = [
+      ".cache"
+      ".local/share/Trash"
+      ".npm"
+      ".cargo"
+      "node_modules"
+      "target"
+      "vendor"
+    ];
+  };
+
+  services.restic.backups.maxHome = {
+    user = "max";
+    initialize = true;
+
+    repository = "s3:https://s3-eu-west-1.wunder.run/mars-backup";
+    environmentFile = "/home/max/.config/restic/env";
+    passwordFile    = "/home/max/.config/restic/password";
+
+    paths = [
+      "/home/max"
+    ];
+
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      RandomizedDelaySec = "30m";
+    };
+
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 4"
+      "--keep-monthly 6"
+    ];
+
+    extraBackupArgs = [
+      "--exclude-caches"
+      "--exclude=**/.cache"
+      "--exclude=**/.local/share/Trash"
+      "--exclude=**/.local/share/Steam"
+      "--exclude=**/.local/share/containers/"
+      "--exclude=**/Faugus"
+      "--exclude=**/.npm"
+      "--exclude=**/.cargo"
+      "--exclude=**/.direnv"
+      "--exclude=**/.venv"
+      "--exclude=**/node_modules"
+      "--exclude=**/vendor"
+      "--exclude=**/target"
+      "--tag=home"
+    ];
+  };
+
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
 }
