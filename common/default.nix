15df1b7a72cd3d67ce98a1089045447486cd2817
Max Heyer <mail@max.gp>
2026-02-11 02:45:53 +0100

    Set default configs for audio

--- common/default.nix
+++ common/default.nix
@@ -136,11 +136,31 @@
   services.udev.packages = [ pkgs.yubikey-personalization ];
 
   services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+
+    extraConfig.pipewire."92-low-latency" = {
+      context.properties = {
+        default.clock.rate = 48000;
+        default.clock.quantum = 512;
+        default.clock.min-quantum = 512;
+        default.clock.max-quantum = 1024;
+      };
+    };
   };
+
+  environment.etc."wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+    bluez_monitor.properties = {
+      ["bluez5.enable-sbc-xq"] = true,
+      ["bluez5.enable-msbc"] = true,
+      ["bluez5.enable-hw-volume"] = true,
+      ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+    }
+  '';
 
   services.displayManager = {
     autoLogin = {
