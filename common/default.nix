5abafb075d63bc252e044d8f087bb2bbd77e6eeb
Max Heyer <mail@max.gp>
2026-02-11 02:15:02 +0100

    Add bazecor

--- common/default.nix
+++ common/default.nix
@@ -79,6 +79,8 @@
 
   programs.fish.enable = true;
 
+  programs.bazecor.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.max = {
     isNormalUser = true;
